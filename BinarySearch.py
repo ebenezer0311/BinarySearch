0d877dc07c8c095f237c9d542cdcd92e489c4fc9
unknown <ebenezermanka1234@gmail.com>
2023-08-19 20:30:33 +0530

    bs on ans

--- BinarySearch.py
+++ BinarySearch.py
@@ -264,6 +264,26 @@ class Solution:
                 right = middle
     
         return nums[left]
+    
+# Sqrt(x)
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        start,end=1,x
+
+        while start<=end:
+            mid=(start+end)//2
+
+            if mid*mid==x:
+                return mid
+            
+            elif mid*mid>x:
+                end=mid-1
+            
+            else:
+                start=mid+1
+        
+        return end
+
 
         
 
